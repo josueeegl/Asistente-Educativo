8b303ed5904ec641a0020342105652ae17edc5e5
BryanIsraelMazariegosAlarcón <bryanisrael76@yahoo.com>
2021-10-18 21:52:48 -0600

    Actualización No. 1

---
+++ web/JS/fnprofe.js
@@ -0,0 +1,8 @@
+const token = document.getElementById('token').innerHTML;
+const correo = document.getElementById('correo').innerHTML;
+const nombre = document.getElementById('nombre').innerHTML;
+const apellido = document.getElementById('apellido').innerHTML;
+console.log(`Token: ${token}`);
+console.log(`correo: ${correo}`);
+console.log(`nombre: ${nombre}`);
+console.log(`apellido: ${apellido}`);
